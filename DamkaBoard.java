e4a063377377c6fc651b08b9a58e744ea56af66f
AbedalsalamAkel <154346157+AbedalsalamAkel@users.noreply.github.com>
2024-01-04 00:37:18 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -4,13 +4,22 @@ public class DamkaBoard {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
+                
                 if ((i + j) % 2 == 0) {
-                    System.out.print("* ");
+                    if (j == n - 1) {
+                        System.out.print("*"); 
+                    } else {
+                        System.out.print("* "); 
+                    }
                 } else {
-                    System.out.print("  ");
+                    if (j == n - 1) {
+                        System.out.print(" "); 
+                    } else {
+                        System.out.print("  "); 
+                    }
                 }
             }
-            System.out.println();
+            System.out.println(); 
         }
     }
 }
